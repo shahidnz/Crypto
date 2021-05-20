5c0ced6d2eb1dd353e842c16a83fb05c29da9929
Shahid Siddiqui <mssiddiqui.nz@gmail.com>
2021-05-20 14:20:07 +1200

    Assignment 1 - padding

---
+++ assignment-1-files/bitwise_or.py
@@ -0,0 +1,19 @@
+"""
+Create a function named bitwise_or which takes two integers,
+prints the integer values, then performs a bitwise OR (different to a boolean OR),
+prints the result of the OR, then returns the result.
+
+NOTE: We can't call this function 'or' because that name is reserved in Python so we call it bitwise_or instead.
+"""
+from helper_functions import binary, FIX_ME
+
+"""
+Using code from the bitwise_and() function (see helper_functions.py), create an additional function called bitwise_or() which perform the bitwise OR operation (using the | character, also known as a pipe)
+"""
+def bitwise_or(integer_a, integer_b):
+    val = integer_a ^ integer_b
+    print (f"{val}")
+
+
+if __name__ == '__main__':
+    bitwise_or(85, 170)
---
+++ assignment-1-files/string_to_hex.py
@@ -0,0 +1,21 @@
+"""
+Create a function which takes a string and prints each character as decimal values corresponding to their ASCII codes.
+
+Hint: Use string_with_spaces.py for help.
+"""
+from helper_functions import FIX_ME
+
+
+def str_to_hex(input_string):
+    for character in input_string:
+        ascii_code = ord(character)
+        result = hex(ascii_code)
+        # Print the result
+        print(f'{result} ', end='')
+    print('')
+
+
+
+if __name__ == '__main__':
+    str_to_hex('hello')  # This should print the line '0x68 0x65 0x6c 0x6c 0x6f'
+
